b16fdddc522871993745d345798fa71c70baa665
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-01 11:24:01 -0700

    componentDidMount on monsters json API

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-        },
-        {
-          name: 'Frank',
-        },
-        {
-          name: 'Jacky',
-        },
-      ]
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => 
+      this.setState(
+        () => {
+          return {monsters: users};
+        },
+        () => {
+          console.log(this.state);
+        }
+    ));
   }
 
   render() {
