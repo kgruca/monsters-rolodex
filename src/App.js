0df14b0d9ab04c49136c25ca5d36f4b9ee3416df
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-01 18:34:49 -0700

    added search field to the this.state object to make monster list reappear after clearing search

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -25,6 +26,11 @@ class App extends Component {
   }
 
   render() {
+
+    const filteredMonsters = this.state.monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input 
@@ -32,18 +38,14 @@ class App extends Component {
           type='search' 
           placeholder='Search monsters' 
           onChange={(event) => {
-            const searchString = event.target.value.toLowerCase(); 
-            const filteredMonsters = this.state.monsters.filter(monster => {
-              return monster.name.toLowerCase().includes(searchString);
-            });
-          
+            const searchField = event.target.value.toLowerCase(); 
             this.setState(() => {
-              return {monsters: filteredMonsters};
+              return { searchField };
             });
           }} 
         />
         {
-          this.state.monsters.map(monster => {
+          filteredMonsters.map(monster => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
