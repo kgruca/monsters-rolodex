0b0fe1ce86ccb5c421b7ff92cf88e6cbbe232f06
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-03 16:56:50 -0700

    destructured monster obj to optimize card-list code; added css for card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import './card-list.styles.css';
 
 class CardList extends Component {
     render() {
@@ -6,13 +7,16 @@ class CardList extends Component {
 
         return (
             <div className="card-list">
-                {monsters.map(monster => (
-                    <div className="card-container" key={monster.id}>
-                        <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-                        <h2>{monster.name}</h2>
-                        <p>{monster.email}</p>
-                    </div>
-                ))}
+                {monsters.map(monster => {
+                    const {name, id, email} = monster;
+                    return (
+                        <div className="card-container" key={id}>
+                            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
