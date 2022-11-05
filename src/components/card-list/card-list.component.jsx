d3ae23d948ed1cd30118c1602f7080b41561253a
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-04 19:10:01 -0700

    app fully converted to functional component from class component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -10,7 +10,7 @@ class CardList extends Component {
             <div className="card-list">
                 {monsters.map(monster => {
                     return (
-                        <Card monster={monster} />
+                        <Card key={monster.id} monster={monster} />
                     );
                 })}
             </div>
